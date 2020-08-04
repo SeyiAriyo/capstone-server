8cc549a4e9654ad9d391ff78cf4cf391856fb624
SeyiAriyo <58642769+SeyiAriyo@users.noreply.github.com>
2020-08-04 18:26:43 -0400

    api keys

--- src/config.js
+++ src/config.js
@@ -4,3 +4,5 @@ module.exports = {
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/recipe-server',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret'
   }
+
+  //password: 46bd4e04f8f00f6c354fb41a99210854133e5748c3f3204912bd7e23cbf5a8c3
