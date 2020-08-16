2ceb1335b665a84e347a7958e311613068bb16a0
SeyiAriyo <58642769+SeyiAriyo@users.noreply.github.com>
2020-08-15 21:39:34 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  PORT: process.env.PORT || 8000,
+  PORT: process.env.PORT || 8080,
   NODE_ENV: process.env.NODE_ENV || 'development', 
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/recipe-server',
   API_TOKEN : process.env.API_TOKEN || null, 
