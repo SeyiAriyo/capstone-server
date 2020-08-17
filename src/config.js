a910bea129c38999aaf77af15cb15c90e05f0f48
SeyiAriyo <58642769+SeyiAriyo@users.noreply.github.com>
2020-08-17 17:48:17 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
   NODE_ENV: process.env.NODE_ENV || 'development', 
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin:password-111@localhost/recipe-server',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin:111@localhost/recipe-server',
   API_TOKEN : process.env.API_TOKEN || null, 
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret'
 }
